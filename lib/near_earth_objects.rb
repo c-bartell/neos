c0311674ce14988f199dae92ce7adb49d88aede9
Curtis Bartell <cbartell980@gmail.com>
2020-11-27 16:47:14 -0700

    Move near_earth_objects.rb to lib

--- lib/near_earth_objects.rb
+++ lib/near_earth_objects.rb
@@ -2,7 +2,7 @@ require 'faraday'
 require 'figaro'
 require 'pry'
 # Load ENV vars via Figaro
-Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
+Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../../config/application.yml', __FILE__))
 Figaro.load
 
 class NearEarthObjects
@@ -14,7 +14,7 @@ class NearEarthObjects
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
-
+    
     parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
 
     largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
