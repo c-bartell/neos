89f4fe7a9795194915b104b51f22134d15f4900e
Curtis Bartell <cbartell980@gmail.com>
2020-11-27 17:03:06 -0700

    Update test_it_exists

--- test/asteroid_test.rb
+++ test/asteroid_test.rb
@@ -6,6 +6,9 @@ class AsteroidTest < Minitest::Test
 
 
   def test_it_exists
+    name = '2011 GE3'
+    diameter = '123 ft'
+    miss_distance = '35542652 miles'
     assert_instance_of Asteroid, Asteroid.new(name, diameter, miss_distance)
   end
 end
