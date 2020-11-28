7a5aab4f121c245f2d4dbec243a9740cbbe8b2d4
Curtis Bartell <cbartell980@gmail.com>
2020-11-28 15:25:55 -0700

    Add test_a_date_also_returns_largest_asteroid_and_number_of_asteroids

--- test/near_earth_objects_test.rb
+++ test/near_earth_objects_test.rb
@@ -5,11 +5,21 @@ require 'json'
 require './lib/near_earth_objects.rb'
 
 class NearEarthObjectsTest < Minitest::Test
-  def test_a_date_returns_a_list_of_neos
-    results = NearEarthObjects.find_neos_by_date('2019-03-30')
+  def setup
+    @results = NearEarthObjects.find_neos_by_date('2019-03-30')
+  end
 
-    assert_equal '(2011 GE3)', results[:asteroid_list][0].name
-    assert_equal 123, results[:asteroid_list][0].diameter
-    assert_equal 35542652, results[:asteroid_list][0].miss_distance
+  def test_a_date_returns_a_list_of_neos
+    # results = NearEarthObjects.find_neos_by_date('2019-03-30')
+
+    assert_equal 10, @results[:asteroid_list].count
+    assert_equal '(2011 GE3)', @results[:asteroid_list][0].name
+    assert_equal 123, @results[:asteroid_list][0].diameter
+    assert_equal 35542652, @results[:asteroid_list][0].miss_distance
+  end
+
+  def test_a_date_also_returns_largest_asteroid_and_number_of_asteroids
+    assert_equal "537 ft", @results[:biggest_asteroid]
+    assert_equal 10, @results[:number_of_asteroids]
   end
 end
