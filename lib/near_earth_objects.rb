ab7089ef7aee9c934d761938512e7ffea1b24844
Curtis Bartell <cbartell980@gmail.com>
2020-11-27 17:22:33 -0700

    Refactor formatted_asteroid_data to contain Asteroid objects

--- lib/near_earth_objects.rb
+++ lib/near_earth_objects.rb
@@ -1,6 +1,7 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
+require_relative 'asteroid.rb'
 # Load ENV vars via Figaro
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../../config/application.yml', __FILE__))
 Figaro.load
@@ -23,11 +24,16 @@ class NearEarthObjects
 
     total_number_of_asteroids = parsed_asteroids_data.count
     formatted_asteroid_data = parsed_asteroids_data.map do |asteroid|
-      {
-        name: asteroid[:name],
-        diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
-      }
+      Asteroid.new(
+          asteroid[:name],
+          "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+          "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+      )
+      # {
+      #   name: asteroid[:name],
+      #   diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+      #   miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+      # }
     end
 
     {
