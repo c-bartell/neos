710a5010c67434b897cbc91645a018c9a8183416
Curtis Bartell <cbartell980@gmail.com>
2020-11-28 15:25:22 -0700

    Refactor with neos_data helper

--- lib/near_earth_objects.rb
+++ lib/near_earth_objects.rb
@@ -8,25 +8,24 @@ Figaro.application = Figaro::Application.new(environment: 'production', path: Fi
 Figaro.load
 
 class NearEarthObjects
-  # This method has too many responsibilities
   def self.find_neos_by_date(date)
-    # This could possibly be a class on it's own, like a request class or something.
-    conn = Faraday.new(
-      url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
-    )
-    asteroids_list_data = conn.get('/neo/rest/v1/feed')
-
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-    # Method should end here.
-
-    asteroids = self.create_asteroids(parsed_asteroids_data)
+    asteroids = self.create_asteroids(self.neos_data(date))
 
     {
       asteroid_list: asteroids.list,
       biggest_asteroid: asteroids.largest_asteroid_diameter,
       total_number_of_asteroids: asteroids.number_of_asteroids
     }
+  end
+
+  def self.neos_data(date)
+    conn = Faraday.new(
+      url: 'https://api.nasa.gov',
+      params: { start_date: date, api_key: ENV['nasa_api_key']}
+    )
+    asteroids_list_data = conn.get('/neo/rest/v1/feed')
+
+    JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
   end
 
   def self.create_asteroids(parsed_asteroids_data)
