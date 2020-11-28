2da2be063d414064799be70d52a2d45e9b11e2c0
Curtis Bartell <cbartell980@gmail.com>
2020-11-28 15:59:22 -0700

    Debug asteroid_list

--- start.rb
+++ start.rb
@@ -1,6 +1,5 @@
 require './lib/near_earth_objects.rb'
-# This needs to be broken into classes.
-# IO class
+
 puts "________________________________________________________________________________________________________________________________"
 puts "Welcome to NEO. Here you will find information about how many meteors, asteroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
 puts "Please enter a date in the following format YYYY-MM-DD."
@@ -8,7 +7,9 @@ print ">>"
 
 date = gets.chomp
 asteroid_details = NearEarthObjects.find_neos_by_date(date)
-asteroid_list = asteroid_details[:asteroid_list]
+asteroid_list = asteroid_details[:asteroid_list].map do |asteroid|
+  asteroid.data_hash
+end
 total_number_of_asteroids = asteroid_details[:total_number_of_asteroids]
 largest_asteroid = asteroid_details[:biggest_asteroid]
 
@@ -34,7 +35,7 @@ end
 formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
 puts "______________________________________________________________________________"
 puts "On #{formated_date}, there were #{total_number_of_asteroids} objects that almost collided with the earth."
-puts "The largest of these was #{largest_asteroid} ft. in diameter."
+puts "The largest of these was #{largest_asteroid}. in diameter."
 puts "\nHere is a list of objects with details:"
 puts divider
 puts header
