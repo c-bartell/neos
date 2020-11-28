3141fef6b6d190ddfd35ad79f19da1725a0ff7dc
Curtis Bartell <cbartell980@gmail.com>
2020-11-27 17:13:04 -0700

    Add attributes

--- lib/asteroid.rb
+++ lib/asteroid.rb
@@ -1,5 +1,9 @@
 class Asteroid
-  def initialize(name, diameter, miss_distance)
+  attr_reader :name, :diameter, :miss_distance
 
+  def initialize(name, diameter, miss_distance)
+    @name = name
+    @diameter = diameter
+    @miss_distance = miss_distance
   end
 end
