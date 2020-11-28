dad7f6e7fe7af47bd22395adbc61e6bfd77845e9
Curtis Bartell <cbartell980@gmail.com>
2020-11-27 17:08:02 -0700

    Refactor test_it_exists with setup

--- test/asteroid_test.rb
+++ test/asteroid_test.rb
@@ -3,12 +3,14 @@ require 'minitest/pride'
 require './lib/asteroid'
 
 class AsteroidTest < Minitest::Test
-
+  def setup
+    @name = '2011 GE3'
+    @diameter = '123 ft'
+    @miss_distance = '35542652 miles'
+    @asteroid = Asteroid.new(@name, @diameter, @miss_distance)
+  end
 
   def test_it_exists
-    name = '2011 GE3'
-    diameter = '123 ft'
-    miss_distance = '35542652 miles'
-    assert_instance_of Asteroid, Asteroid.new(name, diameter, miss_distance)
+    assert_instance_of Asteroid, @asteroid
   end
 end
